057ac472c2331b52b1fde9c97df4ba066bf5aa64
Jochem van Weelde <jochem-van-weelde@hotmail.nl>
2022-02-16 15:14:27 +0100

    Token lexer complete (with line and char data)

---
+++ Interpreter/ClassLexer.py
@@ -0,0 +1,167 @@
+from typing import List
+from ClassToken import *
+from copy import *
+
+class Lexer:
+    def __init__(self, text: str) -> None:
+        self.text = text
+        self.pos = 0
+        self.token_data = TokenData(1,1)
+        self.current_token = None
+
+
+    def error(self) -> Exception:
+        raise Exception("Error!")
+
+    def add_pos(self, adder: int):
+        self.pos += adder
+        self.token_data.char += adder
+    
+    def add_new_line(self):
+        self.add_pos(1)
+        self.token_data.line += 1
+        self.token_data.char = 1
+
+    def is_eof(self, text: str, pos: int) -> bool:
+        return pos > len(text) - 1
+
+    def get_next_token(self) -> Token:
+        if self.pos > len(self.text) - 1:
+            return Token(TokenType.EOF, None, self.token_data)
+        
+        current_char = self.text[self.pos]
+        current_token_data = copy(self.token_data)
+
+        if current_char.isdigit():
+            digit = current_char + self.get_next_digit(self.text, self.pos+1)
+            self.add_pos(len(digit))
+            if digit.count('.') == 0:
+                return Token(TokenType.DIGIT, int(digit), current_token_data)
+            if digit.count('.') == 1:
+                return Token(TokenType.DIGIT, float(digit), current_token_data)
+            raise Exception("Wrong number format: " + digit)
+
+        elif current_char.isalpha():
+            word = current_char + self.get_next_word(self.text, self.pos+1)
+            self.add_pos(len(word))
+
+            match word:
+                case TokenType.BOOL.value:
+                    return Token(TokenType.BOOL, None, current_token_data)
+                case TokenType.INT.value:
+                    return Token(TokenType.INT, None, current_token_data)
+                case TokenType.FLOAT.value:
+                    return Token(TokenType.FLOAT, None, current_token_data)
+                case TokenType.STRING.value:
+                    return Token(TokenType.STRING, None, current_token_data)
+                case TokenType.TRUE.value:
+                    return Token(TokenType.TRUE, None, current_token_data)
+                case TokenType.FALSE.value:
+                    return Token(TokenType.FALSE, None, current_token_data)
+                case TokenType.ADDIDION.value:
+                    return Token(TokenType.ADDIDION, None, current_token_data)
+                case TokenType.SUBTRACTION.value:
+                    return Token(TokenType.SUBTRACTION, None, current_token_data)
+                case TokenType.MULTIPLICATION.value:
+                    return Token(TokenType.MULTIPLICATION, None, current_token_data)
+                case TokenType.DIVISION.value:
+                    return Token(TokenType.DIVISION, None, current_token_data)
+                case TokenType.IS.value:
+                    return Token(TokenType.IS, None, current_token_data)
+                case TokenType.EQUALTO.value:
+                    return Token(TokenType.EQUALTO, None, current_token_data)
+                case TokenType.NOTEQUAL.value:
+                    return Token(TokenType.NOTEQUAL, None, current_token_data)
+                case TokenType.GREATERTHAN.value:
+                    return Token(TokenType.GREATERTHAN, None, current_token_data)
+                case TokenType.GREATERTHANOREQUALTO.value:
+                    return Token(TokenType.GREATERTHANOREQUALTO, None, current_token_data)
+                case TokenType.LESSTHAN.value:
+                    return Token(TokenType.LESSTHAN, None, current_token_data)
+                case TokenType.LESSTHANOREQUALTO.value:
+                    return Token(TokenType.LESSTHANOREQUALTO, None, current_token_data)
+                case TokenType.LOGICALNOT.value:
+                    return Token(TokenType.LOGICALNOT, None, current_token_data)
+                case TokenType.LOGICALAND.value:
+                    return Token(TokenType.LOGICALAND, None, current_token_data)
+                case TokenType.LOGICALOR.value:
+                    return Token(TokenType.LOGICALOR, None, current_token_data)
+                case TokenType.IF.value:
+                    return Token(TokenType.IF, None, current_token_data)
+                case TokenType.ELSEIF.value:
+                    return Token(TokenType.ELSEIF, None, current_token_data)
+                case TokenType.ELSE.value:
+                    return Token(TokenType.ELSE, None, current_token_data)
+                case TokenType.WHILE.value:
+                    return Token(TokenType.WHILE, None, current_token_data)
+                case TokenType.PRINT.value:
+                    return Token(TokenType.PRINT, None, current_token_data)
+                case TokenType.RETURN.value:
+                    return Token(TokenType.RETURN, None, current_token_data)
+            return Token(TokenType.VARIABLE, word, current_token_data)
+
+        elif current_char.isspace():
+            if current_char == '\n':
+                self.add_new_line()
+            else:
+                self.add_pos(1)
+            return self.get_next_token()
+        
+
+        match current_char:
+            case TokenType.LEFTROUNDBRACKET.value:
+                self.add_pos(1)
+                return Token(TokenType.LEFTROUNDBRACKET, None, current_token_data)
+            case TokenType.RIGHTROUNDBRACKET.value:
+                self.add_pos(1)
+                return Token(TokenType.RIGHTROUNDBRACKET, None, current_token_data)
+            case TokenType.LEFTSQUAREBRACKET.value:
+                self.add_pos(1)
+                return Token(TokenType.LEFTSQUAREBRACKET, None, current_token_data)
+            case TokenType.RIGHTSQUAREBRACKET.value:
+                self.add_pos(1)
+                return Token(TokenType.RIGHTSQUAREBRACKET, None, current_token_data)
+            case TokenType.LEFTANGLEBRACKET.value:
+                self.add_pos(1)
+                return Token(TokenType.LEFTANGLEBRACKET, None, current_token_data)
+            case TokenType.RIGHTANGLEBRACKET.value:
+                self.add_pos(1)
+                return Token(TokenType.RIGHTANGLEBRACKET, None, current_token_data)
+            case TokenType.LEFTCURLYBRACKET.value:
+                self.add_pos(1)
+                return Token(TokenType.LEFTCURLYBRACKET, None, current_token_data)
+            case TokenType.RIGHTCURLYBRACKET.value:
+                self.add_pos(1)
+                return Token(TokenType.RIGHTCURLYBRACKET, None, current_token_data)
+            case TokenType.COMMA.value:
+                self.add_pos(1)
+                return Token(TokenType.COMMA, None, current_token_data)
+            case TokenType.SEMICOLON.value:
+                self.add_pos(1)
+                return Token(TokenType.SEMICOLON, None, current_token_data)
+
+        raise Exception("Character not recognised: " + current_char + current_token_data.__str__())
+
+    def get_next_digit(self, text: str, pos: int) -> str:
+        # add chars if next char is a digit or a dot (floats)
+        if not self.is_eof(text, pos) and (text[pos].isdigit() or text[pos] == '.'):
+            return text[pos] + self.get_next_digit(text, pos+1)
+        return ''
+
+    def get_next_word(self, text: str, pos: int) -> str:
+        if not self.is_eof(text, pos) and text[pos].isalpha():
+            return text[pos] + self.get_next_word(text, pos+1)
+        return ''
+
+if __name__ == "__main__":
+    with open('dutchPlusPlus.txt', 'r') as file:
+        data = file.read()
+    a = Lexer(data)
+    current_tokens : List[TokenType] = []
+    current_tokens.append(a.get_next_token())
+    while not current_tokens[-1].__str__().__contains__('EOF'):
+        current_tokens.append(a.get_next_token())
+    
+    for token in current_tokens:
+        print(token)
+    
--- Interpreter/ClassToken.py
+++ Interpreter/ClassToken.py
@@ -1,22 +1,79 @@
+from dataclasses import dataclass
+from enum import Enum
+
 #====ALL TOKENS====
-GETAL   = 'GETAL'     #int
-ZWEEF   = 'ZWEEF'     #float
-WOORD   = 'WOORD'     #variable
-PLUS    = 'PLUS'      # +
-MIN     = 'MIN'       # -
-KEER    = 'KEER'      # *
-DEEL    = 'DEEL'      # /
-EOF     = 'EOF'       #EOF
+class TokenType(Enum):
+    #Data Types Tokens
+    BOOL      = 'janee'           # bool
+    INT       = 'getal'           # int
+    FLOAT     = 'zweef'           # float
+    STRING    = 'woord'           # string
+
+    #Constants Tokens
+    TRUE    = 'waar'    # true
+    FALSE   = 'onwaar'  # false
+
+    #Arithmetic Operator Tokens
+    ADDIDION        = 'plus'            # +
+    SUBTRACTION     = 'min'             # -
+    MULTIPLICATION  = 'keer'            # *
+    DIVISION        = 'delendoor'       # /
+
+    #Assignment Operator Tokens
+    IS  = 'is'  # =
+
+    #Comparison Operator Tokens
+    EQUALTO                 = 'gelijkaan'           # ==
+    NOTEQUAL                = 'nietgelijkaan'       # !=
+    GREATERTHAN             = 'groterdan'           # >
+    GREATERTHANOREQUALTO    = 'groterofgelijkaan'   # >=
+    LESSTHAN                = 'kleinerdan'          # <
+    LESSTHANOREQUALTO       = 'kleinerofgelijkaan'  # <=
+
+    #Logical Operator Tokens
+    LOGICALNOT      = 'niet'    # !
+    LOGICALAND      = 'en'      # &&
+    LOGICALOR       = 'of'      # ||
+
+    #Special Chars Tokens
+    LEFTROUNDBRACKET    = '('   # (
+    RIGHTROUNDBRACKET   = ')'   # )
+    LEFTSQUAREBRACKET   = '['   # [
+    RIGHTSQUAREBRACKET  = ']'   # ]
+    LEFTANGLEBRACKET    = '<'   # <
+    RIGHTANGLEBRACKET   = '>'   # >
+    LEFTCURLYBRACKET    = '{'   # {
+    RIGHTCURLYBRACKET   = '}'   # }
+    COMMA               = ','   # ,
+    SEMICOLON           = ';'   # ;
+
+    #Other Tokens
+    IF          = 'als'         # if
+    ELSEIF      = 'andersals'   # else if
+    ELSE        = 'anders'      # else
+    WHILE       = 'terwijl'     # while
+    PRINT       = 'print'       # cout <<
+    RETURN      = 'geefterug'   # return
+    VARIABLE    = any           # name of variable
+    DIGIT       = any           # any digit
+    EOF         = 'EVB'         # EOF
+
+@dataclass
+class TokenData:
+    line: int
+    char: int
 
 class Token:
-    def __init__(self, type: str, value: str) -> None:
+    def __init__(self, type: TokenType, value: str, token_data: TokenData) -> None:
         self.type = type
         self.value = value
+        self.token_data = token_data
 
     def __str__(self) -> str:
-        return 'Token({type}, {value})'.format(
+        return 'Token({type}, {value}, {token_data})'.format(
             type=self.type,
-            value=repr(self.value)
+            value=repr(self.value),
+            token_data=repr(self.token_data)
         )
     
     def __repr__(self) -> str:
